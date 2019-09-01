9985bb2d4ad68309b71c9b82b3eb3ba466c266bf
chalanika <chalanikasewwandi@gmail.com>
2019-09-01 12:52:46 +0530

    users component

--- Project/NodeJs/controller/empContr.js
+++ Project/NodeJs/controller/empContr.js
@@ -2,14 +2,14 @@ const express = require('express');
 var router = express.Router();
 
 var { Employee } = require('../models/employee');
-
+/*
 router.get('/', (req, res) => {
     Employee.find((err, docs) => {
         if (!err) { res.send(docs); } else {
             console.log('Err');
         }
     });
-});
+});*/
 
 router.post('./', (req, res) => {
     var emp = new Employee({
@@ -18,7 +18,20 @@ router.post('./', (req, res) => {
     });
     emp.save((err, doc) => {
         if (!err) { res.send(doc); } else { console.log("err"); }
-    })
-})
+    });
+});
+/*
+router.put('./:id',(req,res)=>{
+    if(!ObjectId.isvalid(req.params.id))
+        return res.status(400).send(`no record`);
 
+    var emp ={
+        name:req.body.name,
+        position:req.body.position,
+    };
+    Employee.findByIdAndUpdate(req.params.id,{$set:emp},{new:true});
+});
+
+
+*/
 module.exports = router;
---
+++ Project/first/src/app/shared/employee.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+}
