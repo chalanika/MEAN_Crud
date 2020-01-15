719519c392eda7aadf37d5f19498f1e893076995
chalanika sewwandi <43123589+chalanika@users.noreply.github.com>
2020-01-15 07:56:58 +0530

    Update index.js
    ddfdfd

--- Project/NodeJs/index.js
+++ Project/NodeJs/index.js
@@ -10,5 +10,4 @@ app.use(bodyParser.json());
 
 app.listen(3000, () => console.log("server start"));
 // app.use('/employees', employeeCntr);
-app.use('/emplo', employeeCntr);
-console.log('kjdydfjcyustjzbxmyjztyxtfz xzbystyuatjxjamnxjuyai nqtyu juk uli li  oisia9i jwystuquwyquiioquoi kjwdsuywuiowji');
+app.use('/emplodepartmen', employeeCntr);
