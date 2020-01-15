32518b2b2e79925bd670d2c62bddc784c846857f
chalanika <chalanikasewwandi@gmail.com>
2020-01-15 07:58:41 +0530

    ffffffff

--- Project/NodeJs/index.js
+++ Project/NodeJs/index.js
@@ -9,6 +9,5 @@ app.use(bodyParser.json());
 
 
 app.listen(3000, () => console.log("server start"));
-// app.use('/employees', employeeCntr);
-app.use('/emplo', employeeCntr);
-console.log('kjdydfjcyustjzbxmyjztyxtfz xzbystyuatjxjamnxjuyai nqtyu juk uli li  oisia9i jwystuquwyquiioquoi kjwdsuywuiowji');
+app.use('/employees', employeeCntr);
+
