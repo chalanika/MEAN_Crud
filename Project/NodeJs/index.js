0a3e24b44fd49e05c614555d38bfe6bc7998c5f1
chalanika <chalanikasewwandi@gmail.com>
2020-01-15 07:32:11 +0530

    checkin pull

--- Project/NodeJs/index.js
+++ Project/NodeJs/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db.js');
+const{ mongoose } = require('./db.js');
 var employeeCntr = require('./controller/empContr.js');
 
 var app = express();
@@ -9,4 +9,6 @@ app.use(bodyParser.json());
 
 
 app.listen(3000, () => console.log("server start"));
-app.use('/employees', employeeCntr);
+// app.use('/employees', employeeCntr);
+app.use('/emplo', employeeCntr);
+console.log('kjdydfjcyustjzbxmyjztyxtfz xzbystyuatjxjamnxjuyai nqtyu juk uli li  oisia9i jwystuquwyquiioquoi kjwdsuywuiowji');
